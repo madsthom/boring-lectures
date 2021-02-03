2fbcac88eddbc3247155e64e8d7e2460ac903f91
Mads Smed <mads.th@hotmail.com>
2021-02-03 21:45:34 +0100

    Added stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SplitterModule} from 'primeng/splitter';
+import {ButtonModule} from 'primeng/button';
+import {ToolbarModule} from 'primeng/toolbar';
+import { EditorComponent } from './components/editor/editor.component';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SplitterModule,
+    ButtonModule,
+    ToolbarModule,
+    SplitButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
